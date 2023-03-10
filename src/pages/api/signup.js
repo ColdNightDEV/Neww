e07df84b1eab03840196f0349bcfdc1ce9791366
Legadax..xo <77586407+ColdNightDEV@users.noreply.github.com>
2023-03-10 02:34:24 +0100

    c

--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -1,19 +1,31 @@
 import { connectToDatabase } from '../../../utils/mongodb';
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email, number } = req.body;
 
-    const { db } = await connectToDatabase();
-    await db.collection('UserData').insertOne({
-      name,
-      email,
-      number
-    });
+    res.setHeader('Access-Control-Allow-Origin', 'https://legaax.vercel.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    res.status(201).json({ message: 'User created' });
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+    // Handle preflight request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
+    // 
+    if (req.method === 'POST') {
+        const { name, email, number } = req.body;
+
+        const { db } = await connectToDatabase();
+            await db.collection('UserData').insertOne({
+            name,
+            email,
+            number
+        });
+
+        res.status(201).json({ message: 'User created' });
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
 }
