5a601c56445b3afa9487c30b80c29f4a80905080
Legadax..xo <77586407+ColdNightDEV@users.noreply.github.com>
2023-03-10 02:43:36 +0100

    x

--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -1,8 +1,30 @@
-import { connectToDatabase } from '../../../utils/mongodb';
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.MONGODB_DB;
+
+let cachedClient = null;
+let cachedDb = null;
+
+export async function connectToDatabase() {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true, connectTimeoutMS: 30000, socketTimeoutMS: 30000 });
+  const db = client.db(dbName);
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
+}
 
 export default async function handler(req, res) {
 
     res.setHeader('Access-Control-Allow-Origin', 'https://legaax.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://legaax.vercel.app/api');
+    res.setHeader('Access-Control-Allow-Origin', 'https://legaax.vercel.app/api/signup');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
