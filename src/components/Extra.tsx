73095eca2ff55499affbbc39fa129b2504d4a76e
Legadax..xo <77586407+ColdNightDEV@users.noreply.github.com>
2023-03-13 05:28:10 +0100

    Chan

--- src/components/Extra.tsx
+++ src/components/Extra.tsx
@@ -8,7 +8,7 @@ const Extra = (props: Props) => {
       {/* Left Side */}
       <div>
         <h1 className=" text-[24px] md:text-[40px] w-[303px] md:w-[430px] m-10 font-semibold md:font-bold">
-          Some of the things you may want to know
+          Some of the things you may want to know...
         </h1>
       </div>
 
@@ -19,7 +19,7 @@ const Extra = (props: Props) => {
           <h1 className="mx-3 text-[14px] font-bold pt-4 pb-3 md:text-[16px]">
             What is GNPay Africa?
           </h1>
-          <p className=" mx-3 w-[280px] leading-[18px] md:w-[556px] text-[12px] md:text-[15px] font-normal">
+          <p className=" mx-3 w-[280px] hidden leading-[18px] md:w-[556px] text-[12px] md:text-[15px] font-normal">
             GN Pay Africa is a buy now, pay later (BNPL) platform that allows
             you to purchase items and pay for them in installments.
           </p>
@@ -30,7 +30,7 @@ const Extra = (props: Props) => {
           <h1 className="mx-3 text-[14px] font-bold pt-4 pb-3 md:text-[16px]">
             What are the benefits of joining the waitlist?
           </h1>
-          <p className="mx-3 w-[300px] leading-[18px] md:w-[556px] text-[12px] md:text-[15px] font-normal">
+          <p className="mx-3 w-[300px] hidden leading-[18px] md:w-[556px] text-[12px] md:text-[15px] font-normal">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
             esse. A unde ut sed perspiciatis repellendus nobis molestiae fugit
             in.
@@ -42,7 +42,7 @@ const Extra = (props: Props) => {
           <h1 className="mx-3 text-[14px] font-bold pt-4 pb-3 md:text-[16px]">
             When will the GNPay Africa mobile app launch?
           </h1>
-          <p className="mx-3 w-[300px] leading-[18px] md:w-[556px] text-[12px] md:text-[15px] font-normal">
+          <p className="mx-3 w-[300px] hidden leading-[18px] md:w-[556px] text-[12px] md:text-[15px] font-normal">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
             esse. A unde ut sed perspiciatis repellendus nobis molestiae fugit
             in.
@@ -52,12 +52,12 @@ const Extra = (props: Props) => {
         {/* Sections */}
         <div className="pb-3 md:pb-10 bg-[#261F43] mt-3 lg:mt-1 px-3 mx-4 md:mx-0">
           <h1 className="mx-3 text-[14px] font-bold pt-4 pb-3 md:text-[16px]">
-            How do I sign up for the BNPL platform?
+            How do I sign up for the BNPL platform? too
           </h1>
-          <p className="mx-3 w-[300px] leading-[18px] md:w-[556px] text-[12px] md:text-[15px] font-normal">
+          <p className="mx-3 w-[300px] hidden leading-[18px] md:w-[556px] text-[12px] md:text-[15px] font-normal">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
             esse. A unde ut sed perspiciatis repellendus nobis molestiae fugit
-            in.
+            in to.
           </p>
         </div>
       </div>
